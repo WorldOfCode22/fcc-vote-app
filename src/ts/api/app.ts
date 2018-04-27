2c3a8abb00a727a567c97d5948041d23ae06d6c0
Sloan <gwaltneys2@winthrop.edu>
2018-04-27 01:39:40 -0400

    added GQL and comments

--- src/ts/api/app.ts
+++ src/ts/api/app.ts
@@ -1,9 +1,27 @@
+/**
+ * Entry point to the application
+ */
 import express from 'express'
+import expressGQL from 'express-graphql'
 import envSelector from './.env/.env'
+import GQLSchema from './graphql/schema'
 
+// get env
 let env = envSelector('DEV')
+
+// start app instance
 let app = express()
 
+/**
+ * middleware for application
+ */
+
+ app.use('/api', expressGQL({
+   graphiql: env.http.GQL.graphiql,
+   schema: GQLSchema
+ }))
+
+// app wait for request
 app.listen(env.http.port, () => {
   console.log('HTTP Server active on port: ' + env.http.port)
 })
