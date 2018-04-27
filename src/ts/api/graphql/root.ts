a713406e781ef6648e9b29d944ceaf1cedbfb38a
Sloan <gwaltneys2@winthrop.edu>
2018-04-27 03:39:31 -0400

    added server status field

--- src/ts/api/graphql/root.ts
+++ src/ts/api/graphql/root.ts
@@ -1,11 +1,21 @@
 /**
  * exports the schema layout for the schema object
  */
-import {GraphQLObjectType} from 'graphql'
-
+import {GraphQLObjectType, GraphQLString} from 'graphql'
+import ServerStatusResolver from './resolvers/server-status'
+import ServerStatusType from './types/server-status-types/server-status'
 export default new GraphQLObjectType({
   name: 'Root_Query',
   fields: {
-
+    ServerStatus: {
+      type: ServerStatusType,
+      args: {
+        service: {type: GraphQLString}
+      },
+      resolve(parentVal, args){
+        console.log(ServerStatusResolver(parentVal, args))
+        return ServerStatusResolver(parentVal, args)
+      }
+    }
   }
 })
