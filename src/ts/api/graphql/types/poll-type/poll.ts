aed8f58b476ab946bd3c3896200905aeebb2100b
Sloan <gwaltneys2@winthrop.edu>
2018-04-28 03:12:29 -0400

    added auth and options fields

--- src/ts/api/graphql/types/poll-type/poll.ts
+++ src/ts/api/graphql/types/poll-type/poll.ts
@@ -3,8 +3,10 @@ import VoteType from './vote-type'
 export default new GraphQLObjectType({
   name: 'Poll',
   fields: {
+    author: {type: GraphQLString},
     id: {type: GraphQLString},
     title: {type: GraphQLString},
+    options: {type: new GraphQLList(GraphQLString)},
     votes: {type: new GraphQLList(VoteType)}
   }
 })
