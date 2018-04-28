08d3031c3aec6b3b1baf790fb3dda176dd214c18
Sloan <gwaltneys2@winthrop.edu>
2018-04-28 05:26:10 -0400

    added mutations to delete users and polls

--- src/ts/api/graphql/mutations.ts
+++ src/ts/api/graphql/mutations.ts
@@ -5,6 +5,8 @@ import createUser from './resolvers/mutations/user-mutations/create-user'
 import createToken from "./resolvers/mutations/token-mutations/create-token";
 import PollType from './types/poll-type/poll'
 import createPoll from './resolvers/mutations/poll-mutations/create-poll'
+import deletePoll from './resolvers/mutations/poll-mutations/delete-poll'
+import deleteUser from "./resolvers/mutations/user-mutations/delete-user";
 export default new GraphQLObjectType({
   name: 'Mutations',
   fields: {
@@ -16,6 +18,16 @@ export default new GraphQLObjectType({
       },
       resolve(parentVal, args){
         return createUser(parentVal, args)
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        token: {type: new GraphQLNonNull(GraphQLString)},
+        id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentVal, args){
+        return deleteUser(parentVal, args)
       }
     },
     createToken: {
@@ -37,6 +49,16 @@ export default new GraphQLObjectType({
       },
       resolve(parentVal, args){
         return createPoll(parentVal, args)
+      }
+    },
+    deletePoll: {
+      type: PollType,
+      args: {
+        token: {type: new GraphQLNonNull(GraphQLString)},
+        id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentVal, args){
+        return deletePoll(parentVal, args)
       }
     }
   }
