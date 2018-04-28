a2360d711b7d54681b79b8b5d469fe843ecd017f
Sloan <gwaltneys2@winthrop.edu>
2018-04-28 03:12:45 -0400

    changed typeo

--- src/ts/api/graphql/types/poll-type/vote-type.ts
+++ src/ts/api/graphql/types/poll-type/vote-type.ts
@@ -3,7 +3,7 @@ import {GraphQLObjectType, GraphQLString, GraphQLList} from 'graphql'
 export default new GraphQLObjectType({
   name: 'Vote',
   fields: {
-    options: {type: GraphQLString},
+    option: {type: GraphQLString},
     usersVoted: {type: new GraphQLList(GraphQLString)}
   }
 })
