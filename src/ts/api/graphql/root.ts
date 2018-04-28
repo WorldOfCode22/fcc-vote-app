f03df4009c6096c969f02d7e5d9a725d470fd965
Sloan <gwaltneys2@winthrop.edu>
2018-04-28 04:21:39 -0400

    added user to query

--- src/ts/api/graphql/root.ts
+++ src/ts/api/graphql/root.ts
@@ -5,6 +5,8 @@ import {GraphQLObjectType, GraphQLString, GraphQLNonNull} from 'graphql'
 import ServerStatusResolver from './resolvers/server-status'
 import ServerStatusType from './types/server-status-types/server-status'
 import PollType from './types/poll-type/poll'
+import UserType from './types/user-type/user'
+import UserResolver from './resolvers/user-resolver'
 export default new GraphQLObjectType({
   name: 'Root_Query',
   fields: {
@@ -22,6 +24,15 @@ export default new GraphQLObjectType({
       type: PollType,
       args: {
         id: {type: new GraphQLNonNull(GraphQLString)}
+      }
+    }, 
+    User: {
+      type: UserType,
+      args: {
+        token: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentVal, args){
+        return UserResolver(parentVal, args)
       }
     }
   }
