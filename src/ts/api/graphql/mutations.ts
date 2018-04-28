f0400c08fbd615b73fb0e94510065562ac05c915
Sloan <gwaltneys2@winthrop.edu>
2018-04-28 03:12:05 -0400

    added create poll mutation

--- src/ts/api/graphql/mutations.ts
+++ src/ts/api/graphql/mutations.ts
@@ -1,8 +1,10 @@
-import { GraphQLObjectType, GraphQLString, GraphQLNonNull } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLList } from "graphql";
 import UserType from './types/user-type/user'
 import TokenType from './types/token-type/token'
 import createUser from './resolvers/mutations/user-mutations/create-user'
 import createToken from "./resolvers/mutations/token-mutations/create-token";
+import PollType from './types/poll-type/poll'
+import createPoll from './resolvers/mutations/poll-mutations/create-poll'
 export default new GraphQLObjectType({
   name: 'Mutations',
   fields: {
@@ -24,6 +26,17 @@ export default new GraphQLObjectType({
       },
       resolve(parentVal, args){
         return createToken(parentVal, args)
+      }
+    },
+    createPoll: {
+      type: PollType,
+      args: {
+        token: {type: new GraphQLNonNull(GraphQLString)},
+        title: {type: new GraphQLNonNull(GraphQLString)},
+        options: {type: new GraphQLNonNull(new GraphQLList(GraphQLString))}
+      },
+      resolve(parentVal, args){
+        return createPoll(parentVal, args)
       }
     }
   }
